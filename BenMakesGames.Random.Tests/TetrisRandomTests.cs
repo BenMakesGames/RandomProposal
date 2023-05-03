6a8a846d3d79d563eb1dcb6bcdb69d5a61562a21
Ben H <ben@benmakesgames.com>
2023-05-02 21:17:43 -0400

    added distribution tests for TetrisRandom

---
+++ BenMakesGames.Random.Tests/TetrisRandomTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace BenMakesGames.Random.Tests;
+
+public class TetrisRandomTests
+{
+    [Fact]
+    public void GetInt_DistributesEvenly()
+    {
+        TestMethods.GetInt_DistributesEvenly(TetrisRandom.Shared);
+    }
+
+    [Fact]
+    public void GetByte_ReturnsAllPossibleValues()
+    {
+        TestMethods.GetByte_ReturnsAllPossibleValues(TetrisRandom.Shared);
+    }
+
+}
